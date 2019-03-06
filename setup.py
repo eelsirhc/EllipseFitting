5db710240210cb837bf44188a434fef0c96ac54b
Christopher Lee <lee@foldmountain.com>
2019-03-06 16:37:34 -0500

    adding setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ellipses',
+      version='0.1',
+      description='Ellipse Fitting',
+      url='http://github.com/eelsirhc/EllipseFitting.git',
+      author='Christopher Lee',
+      author_email='lee@foldmountain.com',
+      license='MIT',
+      packages=['ellipses'],
+      zip_safe=False)
